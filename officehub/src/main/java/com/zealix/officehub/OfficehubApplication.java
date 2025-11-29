8bda2fa794e6698cd6e750271d7558525eaf8882
abhijit-finzly <abhijit.mali@finzly.com>
2025-11-29 23:47:08 +0530

    Initial commit: OfficeHub - Employee Asset Management System with Angular UI and Spring Boot backend

---
+++ officehub/src/main/java/com/zealix/officehub/OfficehubApplication.java
@@ -0,0 +1,13 @@
+package com.zealix.officehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficehubApplication.class, args);
+	}
+
+}
